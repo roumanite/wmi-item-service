96b53bd8b1bff400777c5599e391898c7c423475
Grace <madyagra@gmail.com>
2021-11-16 20:45:04 +0800

    Add server time

--- internal/httpd/liveness_get.go
+++ internal/httpd/liveness_get.go
@@ -2,6 +2,7 @@ package httpd
 
 import (
 	"net/http"
+	"time"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +10,7 @@ func (s *Server) LivenessGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"code": "success",
+			"server_time": time.Now(),
 		})
 	}
 }
