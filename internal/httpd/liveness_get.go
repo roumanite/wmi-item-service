618cfe5ad727dd4027041f26009085aa6eb20523
Grace <madyagra@gmail.com>
2021-11-27 07:45:27 +0800

    Add message

--- internal/httpd/liveness_get.go
+++ internal/httpd/liveness_get.go
@@ -10,6 +10,7 @@ func (s *Server) LivenessGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"code": "success",
+			"message": "Service is up and running!",
 			"server_time": time.Now(),
 		})
 	}
